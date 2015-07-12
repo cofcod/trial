06a2a33f43c582e20fb27e7525387da571b098b2
커피와코드 <cofcod@hatemogi.com>
2015-07-12 14:56:39 +0900

    edit base html5 layout

--- src/cofcod/views.clj
+++ src/cofcod/views.clj
@@ -1,16 +1,33 @@
 (ns cofcod.views
   (:use [hiccup.core]
-        [hiccup.page]))
+        [hiccup.page]
+        [hiccup.element]))
+
+(defn common-head []
+  [:head
+   [:title "커피와코드"]
+   (include-css "/css/bootstrap.min.js" "/css/test.css")
+   (include-js "/js/jquery.min.js" "/js/bootstrap.min.js"
+               "/js/vue.min.js" "/js/test.js")])
+
+(defn navigation []
+  [:nav ""])
+
+(defn footer []
+  [:footer "copyright(c) 2015"
+   (google-analytics)])
+
+(defn google-analytics []
+  (javascript-tag ""))
 
 (defn layout [& content]
   (html5 {:lang "ko"}
-         [:head
-          [:title "커피와코드"]
-          (include-css "test.css")
-          (include-js "test.js")]
-         [:body content]))
+         (common-head)
+         [:body
+          (navigation)
+          [:main content]
+          (footer)]))
 
 (defn index []
   (layout [:h1 "환영합니다!"]
-          [:div "to codenamu!"])
-  (/ 10 0))
+          [:div "to codenamu!"]))
